718779f85b053608c267f1260e716f55e2ba8246
AaronSchwenke <AaronSchwenke@knights.ucf.edu>
2023-03-30 16:13:11 -0400

    Creating some boiler plate code for the api file.

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Test')
+})
+
+app.listen(port, () => {
+    console.log('Application listening on port ${port}');
+})
